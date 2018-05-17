0cf82bb4b2c03acc0f1dae2938179ea3ca2af10c
OAlienO <jeffrey6910@gmail.com>
2018-05-17 16:10:05 +0800

    first commit

---
+++ genflag.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import re
+import sys
+import string
+import random
+
+if len(sys.argv) != 2:
+    print("USAGE : ./genflag.py FLAG{xxxxx}")
+    exit(0)
+
+flag = sys.argv[1]
+
+candidates = {}
+
+for c in string.ascii_lowercase:
+    candidates[c] = set([c, c.upper()])
+    candidates[c.upper()] = set([c, c.upper()])
+
+for c in string.digits:
+    candidates[c] = set([c])
+
+def link(x, y):
+    candidates[x].add(y)
+    candidates[y].add(x)
+
+link('o', '0')
+link('O', '0')
+link('l', '1')
+link('e', '3')
+link('a', '4')
+
+m = re.match("(.*{)(.*)(})", flag)
+if m:
+    prefix, flag, postfix = m.group(1), m.group(2), m.group(3)
+else:
+    prefix, flag, postfix = '', flag, ''
+
+result = ''
+for c in flag:
+    result += random.sample(candidates[c], 1)[0]
+
+print(prefix + result + postfix)
